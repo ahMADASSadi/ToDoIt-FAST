05b26586b2c149e3b5d1092f1b8c21cd720168c0
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-05-03 23:57:27 +0330

    🗃️ Add 'task' schemas
    task schemas for CRUD operations added

---
+++ schemas/todo.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional
+
+from enum import Enum
+
+
+class TaskPriority(str, Enum):
+    low = "low"
+    medium = "medium"
+    high = "high"
+
+
+class TaskBase(BaseModel):
+    title: str
+    description: str
+    priority: TaskPriority = TaskPriority.medium
+
+    class Config:
+        validate_by_name = True
+        json_encoders = {
+            datetime: lambda v: v.isoformat()
+        }
+
+
+class TaskResponse(TaskBase):
+    pk: int = Field(...)
+    created_at: datetime
+    is_finished: bool
+
+
+class TaskCreate(TaskBase):
+    ...
+
+
+class TaskUpdate(TaskBase):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    priority: Optional[TaskPriority] = None
+    is_finished: Optional[bool] = None
